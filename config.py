4074d8d35a89f8fdf01b53ff943b4a8e95ee6fbe
Diego Calderon <diegoac2@illinois.edu>
2022-02-10 15:39:09 -0600

    removed db url

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ class Settings(BaseSettings):
     main_path: str = os.path.abspath(os.path.dirname(__file__))
     models_path: str = os.path.join(main_path, "labels")
     routers_path: str = os.path.join(main_path, "routers")
-    postgres_db_url: str = "postgresql://fast:fast@localhost:5432/geostream"
+    postgres_db_url: str = ""
 
 
 settings = Settings()
